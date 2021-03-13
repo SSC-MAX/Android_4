949073c377c761189d6567b90b446769786cf961
Tony <tony@gmail.com>
2021-03-13 23:02:00 +0800

    MyCloud

--- MyCloud/app/src/main/java/com/example/mycloud/beans/PictureDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mycloud.beans
-
-class PictureDTO {
-    private val picId: String? = null
-    private val name: String? = null
-    private val url: String? = null
-    private val owerId: String? = null
-}
--- MyCloud/app/src/main/java/com/example/mycloud/beans/Pictures.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mycloud.beans
-
-import android.graphics.Bitmap
-
-class Pictures(val id:String, val name:String, val icon:Bitmap?) {
-}
